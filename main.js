34a79464a89d719aa1d7ae9f7716579bffa677cc
Takahiro-Shirahashi <37147042+Takahiro-Shirahashi@users.noreply.github.com>
2019-04-30 01:37:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+//0〜5までの数値をランダムに作成
+var number = Math.floor(Math.random() * 6);
+//入力された数値を整数に変換して、変数answerに代入
+var answer = parseInt(window.prompt('数当てゲーム。0〜5の数字を入力して下さい。'));
 
+//変数を宣言
+var message;
+
+//入力された値に応じたメッセージをwindowsオブジェクトのalertメソッドで表示
+if(answer === number){
+  massage = 'あたり！';
+}
+else if(answer < number){
+   massage = 'はずれ！もっと大きな数です。';
+ }
+else if(answer > number){
+  massage = 'はずれ！もっと小さな数です。';
+}
+else{
+  massage = '0~5の数字を入力して下さい。';
+}
+window.alert(massage);
+
+//答えを知りたい人の為のアレンジ
+document.getElementById('result').textContent = '今回の正解は'+number+'でした！';
